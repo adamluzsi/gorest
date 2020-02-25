8a8c19321a3dd80e266dfbfb5bbfc49e5e6cdadb
Adam Luzsi <adamluzsi@gmail.com>
2020-02-25 23:47:09 +0100

    dead code elimination

--- DefaultContextHandler.go
+++ DefaultContextHandler.go
@@ -5,9 +5,6 @@ import "context"
 type DefaultContextHandler struct{ ContextKey interface{} }
 
 func (d DefaultContextHandler) ContextWithResource(ctx context.Context, resourceID string) (context.Context, bool, error) {
-	if resourceID == `` {
-		return ctx, false, nil
-	}
 	return context.WithValue(ctx, d.ContextKey, resourceID), true, nil
 }
 
