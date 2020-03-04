d7a53f1a6f1617a57151969974a345a5b64a7a5e
Adam Luzsi <adamluzsi@gmail.com>
2020-03-04 22:17:41 +0100

    docs

--- example_handler_handle_test.go
+++ example_handler_handle_test.go
@@ -9,5 +9,7 @@ import (
 func ExampleHandler_Handle() {
 	handler := gorest.NewHandler(TestController{})
 	var myCustomOperationHandler http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+	//
+	// this will register a handler for /{resource-id}/my-custom-operation
 	handler.Handle(`/my-custom-operation`, myCustomOperationHandler)
 }
